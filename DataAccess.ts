e276cc8bb7bfd486057bedc62a6b5bb108f76d49
Aditya Ganti <adi10rox@gmail.com>
2024-02-16 23:11:25 -0800

    add Data Access layer

---
+++ DataAccess.ts
@@ -0,0 +1,28 @@
+import Mongoose from "mongoose";
+require('dotenv').config();
+
+class DataAccess {
+    static mongooseInstance: any;
+    static mongooseConnection: Mongoose.Connection;
+    static DB_CONNECTION_STRING:string = process.env.MONGODB_CONNECTION_STRING;
+
+    
+    constructor () {
+        DataAccess.connect();
+    }
+    
+    static connect (): Mongoose.Connection {
+        if(this.mongooseInstance) return this.mongooseInstance;
+        
+        this.mongooseConnection  = Mongoose.connection;
+        this.mongooseConnection.on("open", () => {
+            console.log("Connected to mongodb.");
+        });
+        
+        this.mongooseInstance = Mongoose.connect(this.DB_CONNECTION_STRING);
+        return this.mongooseInstance;
+    }
+    
+}
+DataAccess.connect();
+export {DataAccess};
